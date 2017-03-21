5329b1bf9ade03b7dd5c7325e6d26f9fcdfbed98
Viviane Pons <viviane.pons@lri.fr>
2017-03-21 17:43:51 +0100

    Adding jqury to slides and some code to get a scrolling slide when it's too big

--- public/js/odk.js
+++ public/js/odk.js
@@ -42,3 +42,24 @@
 	}
     });
 })();
+
+/* Reveal scrolling for long slides */
+
+function resetSlideScrolling(slide) {
+    $(slide).removeClass('scrollable-slide');
+}
+
+function handleSlideScrolling(slide) {
+    if ($(slide).height() >= 800) {
+        $(slide).addClass('scrollable-slide');
+    }
+}
+
+Reveal.addEventListener('ready', function (event) {
+    handleSlideScrolling(event.currentSlide);
+});
+
+Reveal.addEventListener('slidechanged', function (event) {
+    resetSlideScrolling(event.previousSlide)
+    handleSlideScrolling(event.currentSlide);
+});
